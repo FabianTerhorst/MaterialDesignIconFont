d776289892e65795fa0fb672dd69185cda5bb82c
fabian <fabian.terhorst@gmail.com>
2020-11-03 15:51:11 +0100

    Fix package

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .target(
             name: "MaterialDesignIconFont",
             dependencies: [],
-            path: "MaterialDesignIconFont"
+            path: "Sources"
         )
     ]
 )
