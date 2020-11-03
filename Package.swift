a6b450be6b072b1acc549e9c96c5504bb52c059f
Fabian Terhorst <fabian.terhorst@gmail.com>
2020-11-03 15:39:01 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.1
+let package = Package(
+    name: "MaterialDesignIconFont",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "MaterialDesignIconFont", targets: ["MaterialDesignIconFont"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "MaterialDesignIconFont",
+            dependencies: [],
+            path: "MaterialDesignIconFont"
+        )
+    ]
+)
